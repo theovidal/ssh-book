f2a5b4e8b23b85e5381b4603dacf6cd88fdc5c63
Exybore <exybore@becauseofprog.fr>
2019-09-19 20:25:52 +0200

    feat: add book saving

--- book/save.go
+++ book/save.go
@@ -1,6 +1,38 @@
 package book
 
-// Save writes into the book modifications made by the user : adding, updating or deleting connections.
-func Save() {
+import (
+	"github.com/exybore/ssh-book/connections"
+	"gopkg.in/yaml.v2"
+	"os"
+)
 
+// Save writes into the book modifications made by the user : adding, updating or deleting connections.
+func Save(list *[]*connections.Connection) (err error) {
+	filename, err := Path()
+	if err != nil {
+		return
+	}
+
+	err = os.Truncate(filename, 0)
+	if err != nil {
+		return
+	}
+
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return
+	}
+
+	data, err := yaml.Marshal(&list)
+	if err != nil {
+		return
+	}
+
+	_, err = file.Write(data)
+	if err != nil {
+		return
+	}
+
+	err = file.Close()
+	return
 }
